b815a594210f371eed978e8017b64bb45d4c9750
aastha200013 <dollyaastha98@gmail.com>
2022-09-02 16:16:45 +0530

    class&objects file

---
+++ class&objects/college.java
@@ -0,0 +1,14 @@
+class college {
+           String colgName = "xaveir pu college";
+           char color = 'B';
+           byte classroom = 6;
+           byte sections = 2;
+           int lecturers = 15;
+           int subject = 6;
+           int attenders = 5;
+           int bus = 1;
+           int labs = 3;
+           int students = 50;
+           
+   
+}
---
+++ class&objects/collegemodify.java
@@ -0,0 +1,25 @@
+class collegemodify {
+                   public static void main(String [] args){
+                   college c = new college ();
+                   college c1 = new college ();
+                   college c2 = new college ();
+                   college c3 = new college ();
+                   college c4 = new college ();
+                   college c5 = new college ();
+                   college c6 = new college ();
+                   college c7 = new college ();
+                   college c8 = new college ();
+                   college c9 = new college ();
+                   
+                    System.out.println(c.colgName);
+                    System.out.println(c1.color);
+                    System.out.println(c2.classroom);
+                    System.out.println(c3.sections);
+                    System.out.println(c4.lecturers);
+                    System.out.println(c5.subject);
+                    System.out.println(c6.attenders);
+                    System.out.println(c7.labs);
+                    System.out.println(c8.bus);
+                    System.out.println(c9.students);
+      }
+}
