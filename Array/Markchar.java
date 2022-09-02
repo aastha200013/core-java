35762108b8ed41a380876b31e9e39b77d386658a
aastha200013 <dollyaastha98@gmail.com>
2022-09-02 16:11:49 +0530

    Array file

---
+++ Array/Markchar.java
@@ -0,0 +1,34 @@
+class marksheet{
+         public static void main(String [] ar){
+                 char name1 = 'A';
+                 char name2 = 'A';
+                 char name3 = 'S';
+                 char name4 = 'T';
+                 char name5 = 'H';
+                 char name6 = 'A';
+                   System.out.println(name1 +""+ name2 +""+ name3 +""+ name4 +""+ name5 +""+ name6);
+
+                 char [] name = {'d','o','l','l','y'};
+                 System.out.println( name );
+                 System.out.println(name [3] );
+
+                 float [] cgpas =  { 4.8f ,7.6f ,5.5f ,8.9f  };
+                 System.out.println( cgpas);
+
+                 int [] marks =  {45 ,56 ,98 ,67 ,55 };
+                 System.out.println( marks);
+
+                 short [] rollno = { 3 ,5 ,6 ,8 ,1 };
+                 System.out.println(rollno);
+
+                 double [] percent =  { 78.2d , 54.8d ,88.6d , 65.8d};
+                 System.out.println(percent);
+
+                 long [] idnum = { 34567l , 4563728l , 87653428l, 56743l };
+                 System.out.println(idnum);
+
+                 boolean [] pass = {true ,false};
+                 System.out.println(pass);
+
+           }
+}
