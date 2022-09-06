75feedcbbb752b14be296e3bc9b45ebc1ee8b68f
aastha200013 <dollyaastha98@gmail.com>
2022-09-06 14:54:05 +0530

    Replacechar file

---
+++ Aptitude question/Replacechar.java
@@ -0,0 +1,11 @@
+class Replacechar{
+      public static void main(String args[]){
+	  
+	       String s1 ="Welcome to home";
+		   String replaceString = s1.replace ('e','a');
+		   
+		   System.out.println(replaceString);
+		   
+		}
+		
+	}
