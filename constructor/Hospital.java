2a634efb498791534e12ff672c5b94e736d6c226
aastha200013 <dollyaastha98@gmail.com>
2022-09-06 09:02:22 +0530

    Hospital file

---
+++ constructor/Hospital.java
@@ -0,0 +1,15 @@
+class Hospital {
+		static String name = "Suraksha";
+		
+		 String location = "Raichur";
+		 boolean isLiftWorks = true;
+		String [] DrProfession = {"cardiology" , "Ophthalmologist" , "Dermatalogist"};
+		String [] DrName = {"Pranesh" , "Rajendra" ,"Kalappa"};
+		short noOfNurses = 80;
+		short noOfRooms = 160;
+        short noOfPatience = 140 ;
+		byte ambulance = 4;
+		byte Experiance = 8;
+		
+	}	
+		  
