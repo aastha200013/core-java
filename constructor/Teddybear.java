56aeda682d61dd779b9798d2b7d0282de39462b2
aastha200013 <dollyaastha98@gmail.com>
2022-09-06 14:07:14 +0530

    Teddybear file

---
+++ constructor/Teddybear.java
@@ -0,0 +1,19 @@
+class Teddybear {
+          public static void main (String [] args)
+		  {
+		  
+		  Teddy t = new Teddy();
+		  System.out.println(t);
+		  System.out.println(t.Teddyname+"/t"+t.Teddycolor+"/t"+t.Weight+"/t"+t.Length);
+		  
+		  Teddy t1 = new Teddy("PANDA");
+		  System.out.println(t1);
+		  System.out.println(t1.Teddyname);
+		  
+		  Teddy t2 = new Teddy("BlackandWhite", 60 );
+		  System.out.println(t2);
+		  System.out.println(t2.Teddycolor+"/t"+t2.Weight);
+		  
+		  }
+		  
+	}	  
