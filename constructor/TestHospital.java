8932f566a6ef30cb7dfd71180af664aff83a79d5
aastha200013 <dollyaastha98@gmail.com>
2022-09-06 08:35:22 +0530

    TestHospital file

---
+++ constructor/TestHospital.java
@@ -0,0 +1,18 @@
+class TestHospital{
+        public static void main (String [] args){
+		    System.out.println(Hospital.name);
+			
+			Hospital suraksha = new Hospital();
+			System.out.println(suraksha.location);
+			System.out.println(suraksha.noOfNurses);
+			System.out.println(suraksha.noOfRooms);
+			System.out.println(suraksha.noOfPatience);
+			System.out.println(suraksha.ambulance);
+			System.out.println(suraksha.Experiance);
+			System.out.println(suraksha.DrName [0]);
+			System.out.println(suraksha.isLiftWorks);
+			System.out.println(suraksha.DrProfession [1]);
+			
+			Hospital suraksha1 = new Hospital();
+		}
+	}	
